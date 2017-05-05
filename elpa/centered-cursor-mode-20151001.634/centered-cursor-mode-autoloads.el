94f91e916533bca81e760b78ff909523e223d5ee
singaraiona <singaraiona@gmail.com>
2017-05-05 15:47:25 +0300

    removed custom-set-faces

---
+++ elpa/centered-cursor-mode-20151001.634/centered-cursor-mode-autoloads.el
@@ -0,0 +1,23 @@
+;;; centered-cursor-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "centered-cursor-mode" "centered-cursor-mode.el"
+;;;;;;  (22796 11441 209407 282000))
+;;; Generated autoloads from centered-cursor-mode.el
+
+(autoload 'centered-cursor-mode "centered-cursor-mode" "\
+Makes the cursor stay vertically in a defined
+position (usually centered).
+
+\(fn &optional ARG)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; centered-cursor-mode-autoloads.el ends here
--- parts/gui.el
+++ parts/gui.el
@@ -1,8 +1,12 @@
 (provide 'gui)
+(setq default-frame-alist '((font . "Liberation Mono-14")))
+(set-frame-font "Liberation Mono-14")
+(require 'centered-cursor-mode)
+(global-centered-cursor-mode +1)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
-(show-paren-mode 1)
 (scroll-bar-mode -1)
+(show-paren-mode 1)
 
 (require 'all-the-icons)
 (load-theme 'sanityinc-tomorrow-eighties t)
@@ -13,20 +17,3 @@
 (setq spaceline-all-the-icons-hide-long-buffer-path t)
 (setq spaceline-all-the-icons-separator-type 'cup)
 (setq spaceline-all-the-icons-slim-render t)
-
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
-  '(custom-safe-themes
-     (quote
-       ("84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" default)))
-  '(package-selected-packages
-     (quote
-       (nlinum helm winum persp-mode eyebrowse ## flycheck-rust flycheck racer powerline company all-the-icons)))
-  '(powerline-default-separator (quote contour)))
-
-(custom-set-faces
-  '(default ((t (:family "Liberation Mono" :foundry "1ASC" :slant normal :weight normal :height 143 :width normal)))))
-
